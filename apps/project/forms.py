d68cb3258e3150beb3e30f962b5a56d42ec90fac
tach <tachterry@gmail.com>
2024-02-24 15:15:28 +0300

    bagfix: add style

---
+++ apps/project/forms.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.models import User
+from django.forms import fields, widgets, Form
+from django.forms import ModelChoiceField
+
+from apps.project.choises import STATUS_CHOICES, PRIORITY_CHOICES
+from apps.project.models import (
+    Task,
+    Project
+)
+
+
+
+class CreateProjectForm(Form):
+    name = fields.CharField(max_length=200)
+    description = fields.CharField(max_length=2000)
+
+    class Meta:
+        model=Project
+        fields=[
+            'name',
+            'discription'
+        ]
